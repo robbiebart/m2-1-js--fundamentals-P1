38ce57ba2a41eeddb4fea22663cce399783d416b
Robbie Bart <adm@AdminisatorsAir.lan>
2020-06-16 00:21:53 -0400

    q 7 done

--- workshop/exercise-8.js
+++ workshop/exercise-8.js
@@ -3,3 +3,13 @@
 // between 0 and 5000
 
 // ANSWER: the number you should see in the console is 3127500
+let sum = 
+for (let num = 1; num <= 5000; num * 4 ) {
+    let counter = 0;
+    if (num <= 5000) {
+        counter = num + counter;
+    } else {
+        console.log(counter);
+    }
+}
+
